311417cd9287a11d1ff52874c057e1ad3353de35
RaspberryPanda <63005513+asdfrv20@users.noreply.github.com>
2021-09-28 04:28:25 +0900

    20210928 Three Dorks commit

--- 14_백트랙킹/15649_N과 M(1).py
+++ 14_백트랙킹/15649_N과 M(1).py
@@ -21,18 +21,65 @@
   * 유망하지 않은 노드에 가지 않는다 = "가지치기(pruning) 한다"
 '''
 
+# try1: 뭔가 복잡... 정보를 찾아보고 다시 해보자
+
+import time
 
 N, M = map(int, input().split())
 
-N_list = list(range(1,N+1))   # 1~N까지의 자연수를 나타냄
-N_index = list(range(M))       # 출력되는 각 자리가 가리키는 N_list의 인덱스 번호
-
+start = time.time()
+N_list = list(range(1,N+1))   # N_list: 1~N까지의 자연수를 나타냄
+index_list = list(range(M))   # index_list: 출력되는 각 자리가 가리키는 N_list의 인덱스 번호
+key = M-1                     # key: 현재 변경대상이 되는 index 번호
+end_seq = N_list[N-M:N]      # end_list: 가장 마지막 수열(끝내기 위한 조건으로 걸기)
+end_seq.reverse()
+seqs = []                      # seq: 현재 수열 저장
 
 while True:
+  # seq 추가하기 
+  seq_temp = []
+  for index in index_list:
+    seq_temp.append(N_list[index])
+  seqs.append(seq_temp)
+
+  # break 조건 걸기
+  if seqs[-1] == end_seq:
+    break
   
+  # 다음 seq 생성을 위한 index_list 만들기
+  index_list[key] += 1
+  while True:
+    if index_list[key] == N:    # <index_list[key]가 N의 범위를 넘어간 경우 예외처리>
+      index_list[key] = 0       # 1) index_list[현재 key]를 0으로 초기화
+      key -= 1                  # 2) 바로 위 우선순위의 index를 key로 지정(key값 1 감소)
+      index_list[key] += 1      # 3) 우선 순위가 하나 높은 index를 1 증가시키기
+      key += 1
+      index_list[key] += 1
+    
 
+    # 최빈값 검사
+    mode = 0
+    for index in set(index_list):   
+      if index_list.count(index) > index_list.count(mode):
+        mode = index
+        break
 
-  # 뽑아낸 숫자들을 한 줄 출력
-  for i in M_list:
-    print(N_list[i], end=' ')
+    if index_list.count(mode) == 1:    # seq 생성 조건(seq생성 while문 break 조건): 모든 인덱스 숫자에 중복이 없다.
+      break
+    else:                              # <중복이 있을 경우 예외처리>
+      key += 1
+
+    if index_list.count(key) != 1: # 중복되는 index가 존재할 경우, 
+      index_list[key] += 1         # index_list[key]를 1만큼 옮기기
+  key = M-1
+
+# 결과 출력
+for seq in seqs:
+  for i in seq:
+    print(i, end=' ')
   print()
+print("time:", time.time()-start)
+
+
+
+  
