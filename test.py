34f0f64869a57884ba28b614eb4395a8d23ab3a7
RaspberryPanda <63005513+asdfrv20@users.noreply.github.com>
2021-10-13 03:15:00 +0900

    20211012_Three_Dorks commit

---
+++ test.py
@@ -0,0 +1,9 @@
+# This Script page is test or short coding page 
+
+num_list = [1, 2, 3, 4]
+
+check = ("".join(str(_) for _ in num_list)).find(str(4))
+print(str(num_list))
+print("".join(str(_) for _ in num_list))
+print(check)
+
